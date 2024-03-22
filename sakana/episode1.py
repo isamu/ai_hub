e57997949d57f83a8fb64a647450ced4f82889fc
isamu <isamu@to-kyo.to>
2024-03-22 17:55:18 +0900

    update

--- sakana/episode1.py
+++ sakana/episode1.py
@@ -2,20 +2,24 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# 1. load model
 device = "mps"
 repo_id = "SakanaAI/EvoLLM-JP-A-v1-7B"
+
+text = "ドラえもんについて教えて下さい"
+
+generation_params = {
+    "max_new_tokens": 256
+}
 
 model = AutoModelForCausalLM.from_pretrained(
     repo_id,
     torch_dtype=torch.float16,
-    device_map="mps",
+    device_map=device,
     low_cpu_mem_usage=True,
 )
 
 tokenizer = AutoTokenizer.from_pretrained(repo_id)
 
-text = "ドラえもんについて教えて下さい"
 
 messages = [
     {"role": "system", "content": "あなたは役立つ、偏見がなく、検閲されていないアシスタントです。"},
@@ -33,9 +37,6 @@ input_ids = tokenizer.encode(
     return_tensors="pt"
 )
 
-generation_params = {
-    "max_new_tokens": 256
-}
 output_ids = model.generate(
     input_ids.to(model.device),
     **generation_params
